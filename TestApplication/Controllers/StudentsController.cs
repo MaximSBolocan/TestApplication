cc2bd1dc152b9f99ef7e9c2c3bf3d65fa2d261f5
MaximSBolocan <wolverene1@gmail.com>
2018-09-09 21:45:22 -0400

    Added Some controllers

---
+++ TestApplication/Controllers/StudentsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TestApplication.Models;
+
+namespace TestApplication.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private TestApplicationContext db = new TestApplicationContext();
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            var students = db.Students.Include(s => s.SClass).Include(s => s.Semester).Include(s => s.STeacher);
+            return View(students.ToList());
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClassID = new SelectList(db.Classes, "ID", "ID");
+            ViewBag.SemesterID = new SelectList(db.Semesters, "ID", "SemesterName");
+            ViewBag.TeacherID = new SelectList(db.Teachers, "ID", "TName");
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,SName,SLastName,SAge,ClassID,TeacherID,SemesterID")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClassID = new SelectList(db.Classes, "ID", "ID", student.ClassID);
+            ViewBag.SemesterID = new SelectList(db.Semesters, "ID", "SemesterName", student.SemesterID);
+            ViewBag.TeacherID = new SelectList(db.Teachers, "ID", "TName", student.TeacherID);
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClassID = new SelectList(db.Classes, "ID", "ID", student.ClassID);
+            ViewBag.SemesterID = new SelectList(db.Semesters, "ID", "SemesterName", student.SemesterID);
+            ViewBag.TeacherID = new SelectList(db.Teachers, "ID", "TName", student.TeacherID);
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,SName,SLastName,SAge,ClassID,TeacherID,SemesterID")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClassID = new SelectList(db.Classes, "ID", "ID", student.ClassID);
+            ViewBag.SemesterID = new SelectList(db.Semesters, "ID", "SemesterName", student.SemesterID);
+            ViewBag.TeacherID = new SelectList(db.Teachers, "ID", "TName", student.TeacherID);
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TestApplication/Models/TestApplicationContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TestApplication.Models
+{
+    public class TestApplicationContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public TestApplicationContext() : base("name=TestApplicationContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<TestApplication.Models.Class> Classes { get; set; }
+
+        public System.Data.Entity.DbSet<TestApplication.Models.Teacher> Teachers { get; set; }
+
+        public System.Data.Entity.DbSet<TestApplication.Models.Student> Students { get; set; }
+
+        public System.Data.Entity.DbSet<TestApplication.Models.Semester> Semesters { get; set; }
+    }
+}
